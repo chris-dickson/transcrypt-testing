148fb2fb0863ebe6cf1e2dbb8397a810829dddcc
cdickson <cdickson@uncharted.software>
2024-06-21 08:22:23 -0400

    prevent from encrypting the gitattributes file

--- add-secret.sh
+++ add-secret.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ "$1" == ".gitattributes" ]; then
+  echo "You can't add .gitattributes to itself"
+  exit 1
+fi
+
 echo "$1  filter=crypt diff=crypt merge=crypt" >> .gitattributes
 git add .gitattributes $1
